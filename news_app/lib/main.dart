9757dd882ec83511a9a23cd14c8aa7dc43547afd
Omar Abdullah <omarmoharam790@gmail.com>
2024-08-29 12:32:09 +0300

    keep updated

--- news_app/lib/main.dart
+++ news_app/lib/main.dart
@@ -1,10 +1,24 @@
+import 'dart:developer';
+
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:news_app/views/home_page.dart';
 
 void main() {
+  getNews();
   runApp(const NewsApp());
 }
+
 // TODO: add splash screen Flutter native splash
+
+// create gitNews response 🔻
+final dio = Dio();
+
+void getNews() async {
+  final reponse = await dio.get(
+      'https://newsapi.org/v2/everything?q=bitcoin&apiKey=6c4b419554ee41059dfd9bf0381ee1c8#');
+  log(reponse.toString());
+}
 
 class NewsApp extends StatelessWidget {
   const NewsApp({super.key});
