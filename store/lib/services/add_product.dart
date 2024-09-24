60931af77e482042e63aae830e211679972b1512
Omar Abdullah <omarmoharam790@gmail.com>
2024-09-24 15:30:03 +0300

    API: PUT Request Implemented

--- store/lib/services/add_product.dart
+++ store/lib/services/add_product.dart
@@ -2,16 +2,23 @@ import 'package:store/helper/api.dart';
 import 'package:store/models/product_model.dart';
 
 class AddProductService {
-  Future<ProductModel> addProduct({required ProductModel product}) async {
-    Map<String, dynamic> data =
-        // ignore: missing_required_param
-        await Api().post(url: 'https://fakestoreapi.com/products', body: {
-      "title": product.title,
-      "price": product.price,
-      "description": product.description,
-      "image": product.image,
-      "category": product.category
-    });
+  Future<ProductModel> addProduct({
+    required String title,
+    required String price,
+    required String description,
+    required String image,
+    required String category,
+  }) async {
+    Map<String, dynamic> data = await Api().post(
+      url: 'https://fakestoreapi.com/products',
+      body: {
+        "title": title,
+        "price": price,
+        "description": description,
+        "image": image,
+        "category": category,
+      },
+    );
     return ProductModel.fromJson(data);
   }
 }
