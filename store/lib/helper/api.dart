ee5ed00aed5569a169ca63a768a27ded0f129080
Omar Abdullah <omarmoharam790@gmail.com>
2024-09-24 12:15:00 +0300

    API: POST Request Implemented

--- store/lib/helper/api.dart
+++ store/lib/helper/api.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 class Api {
@@ -13,5 +14,27 @@ class Api {
       throw Exception(
           "There was a problem in the status code ${response.statusCode}");
     }
+  }
+
+  Future<dynamic> post(
+      {required String url,
+      @required dynamic body,
+      @required String? token}) async {
+    Map<String, String> headers = {};
+    if (token != null) {
+      headers.addAll(
+        {
+          'Authorization': 'Bearer $token',
+        },
+      );
+    }
+
+    http.Response response = await http.post(
+      Uri.parse(url),
+      body: body,
+      headers: headers,
+    );
+    Map<String, dynamic> productData = jsonDecode(response.body);
+    return productData;
   }
 }
