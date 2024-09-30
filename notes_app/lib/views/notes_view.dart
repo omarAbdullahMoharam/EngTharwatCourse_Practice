8c1db75fff2b058f4e1404269e7925e8f94b8c4d
Omar Abdullah <omarmoharam790@gmail.com>
2024-09-30 13:58:43 +0300

    UI: Handle Theme and NotesView Initialized

---
+++ notes_app/lib/views/notes_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NotesView extends StatelessWidget {
+  const NotesView({super.key});
+  static String notesID = 'NotesView';
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
