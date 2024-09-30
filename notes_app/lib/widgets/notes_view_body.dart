2031d6b453370bcb2c5e0a40b16fa50218387fc8
Omar Abdullah <omarmoharam790@gmail.com>
2024-09-30 17:19:49 +0300

    Padding: Horizontal Padding Increased

--- notes_app/lib/widgets/notes_view_body.dart
+++ notes_app/lib/widgets/notes_view_body.dart
@@ -10,7 +10,7 @@ class NotesViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 12, vertical: 20),
+      padding: EdgeInsets.symmetric(horizontal: 24, vertical: 20),
       child: Column(
         children: [
           SizedBox(
