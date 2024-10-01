d134f9cc4caf5af49ec81b0d0c52fe6a89f6ec8b
Omar Abdullah <omarmoharam790@gmail.com>
2024-10-01 21:58:54 +0300

    UI: Modal Show Bottom Sheet Implemented

--- notes_app/lib/views/notes_view.dart
+++ notes_app/lib/views/notes_view.dart
@@ -14,7 +14,7 @@ class NotesView extends StatelessWidget {
         onPressed: () {
           showModalBottomSheet(
             context: context,
-            builder: (context) => Container(),
+            builder: (context) => const AddNote(),
           );
         },
         child: const Icon(
@@ -31,6 +31,81 @@ class AddNote extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 8),
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 12, bottom: 18),
+            child: TextField(
+              enabled: true,
+              decoration: InputDecoration(
+                hintText: 'Title',
+                hintStyle: const TextStyle(
+                  color: Colors.teal,
+                  fontSize: 18,
+                ),
+                alignLabelWithHint: true,
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: const BorderSide(
+                    color: Colors.white,
+                    width: 1,
+                  ),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: const BorderSide(
+                    color: Colors.white,
+                    width: 2,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          TextField(
+            decoration: InputDecoration(
+              contentPadding:
+                  const EdgeInsets.symmetric(vertical: 60, horizontal: 8),
+              filled: true,
+              fillColor: Colors.grey[900],
+              hintText: 'Content',
+              hintStyle: const TextStyle(color: Colors.teal),
+              enabledBorder: const OutlineInputBorder(
+                borderSide: BorderSide(
+                  color: Colors.white,
+                  width: 1.0,
+                ),
+              ),
+              focusedBorder: const OutlineInputBorder(
+                borderSide: BorderSide(
+                  color: Colors.white,
+                  width: 2.0,
+                ),
+              ),
+            ),
+            style: const TextStyle(color: Colors.white),
+          ),
+          const Spacer(),
+          MaterialButton(
+            padding: const EdgeInsets.symmetric(vertical: 15),
+            onPressed: () {},
+            minWidth: double.infinity,
+            color: Colors.teal,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8),
+            ),
+            splashColor: Colors.tealAccent.shade700,
+            child: const Text(
+              'Add',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 18,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
