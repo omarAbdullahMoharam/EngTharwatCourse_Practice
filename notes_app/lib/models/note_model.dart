b98f39eeaf5ca8cb9e5afd4ab4d902a860f67edf
Omar Abdullah <omarmoharam790@gmail.com>
2024-10-04 09:44:56 +0300

    HiveTypeAdapter Generated Using build_runner

--- notes_app/lib/models/note_model.dart
+++ notes_app/lib/models/note_model.dart
@@ -1,11 +1,26 @@
-class NoteModel {
+import 'package:hive/hive.dart';
+// give the generator the file name to generate TypeAdapter for
+// Using The Keyword part'(Name for the model you need to store its feilds in THe Hive DataBase)'
+part 'note_model.g.dart';
+
+// give the model object type to be TybeAdapted for Hive
+@HiveType(typeId: 0)
+// extends provides you come features to ease the proccess
+class NoteModel extends HiveObject {
+  // Give each field a field (index)-->(ID) To generate the box fields correctly
+
+  @HiveField(0)
   final String title;
+  @HiveField(1)
   final String subTitle;
+  @HiveField(2)
   final String date;
+  @HiveField(3)
   final int color;
-  NoteModel(
-      {required this.title,
-      required this.subTitle,
-      required this.date,
-      required this.color});
+  NoteModel({
+    required this.title,
+    required this.subTitle,
+    required this.date,
+    required this.color,
+  });
 }
