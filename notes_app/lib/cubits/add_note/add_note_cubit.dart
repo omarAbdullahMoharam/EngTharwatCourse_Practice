2468b4db4fe22a8cb63da5564594b02f30590a69
Omar Abdullah <omarmoharam790@gmail.com>
2024-10-05 23:21:55 +0300

    DB: AddNote Method Logic implemented

--- notes_app/lib/cubits/add_note/add_note_cubit.dart
+++ notes_app/lib/cubits/add_note/add_note_cubit.dart
@@ -1,8 +1,25 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive/hive.dart';
+import 'package:notes_app/constants.dart';
+import 'package:notes_app/models/note_model.dart';
 
 part 'add_note_state.dart';
 
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
+
+  addNote(NoteModel note) async {
+    emit(AddNoteLoading());
+    try {
+      var notesBox = Hive.box<NoteModel>(kNotesBox);
+      emit(AddNoteSuccess('Note Added😊'));
+      await notesBox.add(note);
+    } catch (error) {
+      emit(AddNoteFailure(error.toString()));
+      log('${error}from add Note method');
+    }
+  }
 }
