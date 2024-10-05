bd19b7bb55e61cdbfcd8ba463d39e7f6b6ab451d
Omar Abdullah <omarmoharam790@gmail.com>
2024-10-05 22:49:49 +0300

    DB: Add Note Adapter Configured

--- notes_app/lib/main.dart
+++ notes_app/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:notes_app/constants.dart';
+import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/views/edit_note_view.dart';
 import 'package:notes_app/views/notes_view.dart';
 
@@ -9,6 +10,12 @@ void main() async {
   await Hive.initFlutter();
   // Open Hive Box (Like Create Collection or Table ) To store Data in
   await Hive.openBox(kNotesBox);
+  // until now you created a typeAdabter and a box named "notes_box"
+  // 🔻before you start to store the data into the DB You need to initialize
+  // the data adapter to be configured to convert data into box adapter as follows
+  // 🔻🔻🔻🔻 by passing the GENERATED TypeAdapter BY BUILD_RUNNER which Takes
+  // a NoteModel
+  Hive.registerAdapter(NoteModelAdapter());
   runApp(const NotesApp());
 }
 
--- notes_app/lib/models/note_model.dart
+++ notes_app/lib/models/note_model.dart
@@ -1,6 +1,6 @@
 import 'package:hive/hive.dart';
 // give the generator the file name to generate TypeAdapter for
-// Using The Keyword part'(Name for the model you need to store its feilds in THe Hive DataBase)'
+// Using The Keyword part'(Name for the model you need to store its fields in THe Hive DataBase)'
 // 🚨🚨🚨Insure that the
 //file name typed CORRECTLY +(.g.dart)
 //at the end of the file name 🚨🚨🚨
@@ -8,9 +8,9 @@ part 'note_model.g.dart';
 
 //  🚨🚨🚨The command you need to use to generate the TypeAdapter file is 🔻🔻🔻
 // flutter packages pub run build_runner build
-// give the model object type to be TybeAdapted for Hive
+// give the model object type to be TypeAdapted for Hive
 @HiveType(typeId: 0)
-// extends provides you come features to ease the proccess
+// extends provides you come features to ease the process
 
 class NoteModel extends HiveObject {
   // Give each field a field (index)-->(ID) To generate the box fields correctly
