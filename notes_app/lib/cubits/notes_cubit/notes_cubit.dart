b6f36a77bf129138b395f7cccb8878956cdb3753
Omar Abdullah <omarmoharam790@gmail.com>
2024-10-08 22:18:31 +0300

    Create FetchNotesMethod And Inithialize NotesCubit

--- notes_app/lib/cubits/notes_cubit/notes_cubit.dart
+++ notes_app/lib/cubits/notes_cubit/notes_cubit.dart
@@ -1,9 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:notes_app/constants.dart';
 import 'package:notes_app/models/note_model.dart';
 
 part 'notes_state.dart';
 
 class NotesCubit extends Cubit<NotesState> {
   NotesCubit() : super(NotesInitial());
+  fetchAllNotes() {
+    try {
+      var notesBox = Hive.box<NoteModel>(kNotesBox);
+      List<NoteModel> notes = notesBox.values.toList();
+      emit(NotesSuccess(notes));
+      return (notes);
+    } catch (e) {
+      emit(
+        NotesFailure(
+          e.toString(),
+        ),
+      );
+    }
+  }
 }
--- notes_app/lib/views/notes_view.dart
+++ notes_app/lib/views/notes_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/cubits/notes_cubit/notes_cubit.dart';
 import 'add_note_view.dart';
 import '../widgets/notes_view_body.dart';
 
@@ -7,21 +9,24 @@ class NotesView extends StatelessWidget {
   static String notesID = 'NotesView';
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: const NotesViewBody(),
-      floatingActionButton: FloatingActionButton(
-        shape: const CircleBorder(),
-        backgroundColor: Colors.teal,
-        onPressed: () {
-          showModalBottomSheet(
-            isScrollControlled: true,
-            context: context,
-            builder: (context) => const AddNote(),
-          );
-        },
-        child: const Icon(
-          Icons.add,
-          color: Colors.black,
+    return BlocProvider(
+      create: (context) => NotesCubit(),
+      child: Scaffold(
+        body: const NotesViewBody(),
+        floatingActionButton: FloatingActionButton(
+          shape: const CircleBorder(),
+          backgroundColor: Colors.teal,
+          onPressed: () {
+            showModalBottomSheet(
+              isScrollControlled: true,
+              context: context,
+              builder: (context) => const AddNote(),
+            );
+          },
+          child: const Icon(
+            Icons.add,
+            color: Colors.black,
+          ),
         ),
       ),
     );
