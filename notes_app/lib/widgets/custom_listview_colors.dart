7131dfa2c93ae4ca854b0e871d6724ce28ffd39f
Omar Abdullah <omarmoharam790@gmail.com>
2024-10-12 14:25:13 +0300

    UI: Colors Pallete Finished

--- notes_app/lib/widgets/custom_listview_colors.dart
+++ notes_app/lib/widgets/custom_listview_colors.dart
@@ -3,18 +3,33 @@ import 'package:flutter/material.dart';
 import '../helpers/constants.dart';
 import 'custom_note_color.dart';
 
-class ScrollableColorsPallet extends StatelessWidget {
+class ScrollableColorsPallet extends StatefulWidget {
   const ScrollableColorsPallet({super.key});
 
   @override
+  State<ScrollableColorsPallet> createState() => _ScrollableColorsPalletState();
+}
+
+class _ScrollableColorsPalletState extends State<ScrollableColorsPallet> {
+  int? currentIndex = 0;
+  @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 120,
+      height: 140,
       child: ListView.builder(
         scrollDirection: Axis.horizontal,
         itemCount: colorPallet.length,
         itemBuilder: (context, index) {
-          return CustomNoteColor(colorIndex: index);
+          return CustomNoteColor(
+              colorIndex: index,
+              isSelected: currentIndex == index,
+              onSelected: () {
+                setState(
+                  () {
+                    currentIndex = index;
+                  },
+                );
+              });
         },
       ),
     );
--- notes_app/lib/widgets/custom_note_color.dart
+++ notes_app/lib/widgets/custom_note_color.dart
@@ -2,19 +2,43 @@ import 'package:flutter/material.dart';
 
 import '../helpers/constants.dart';
 
-class CustomNoteColor extends StatelessWidget {
-  const CustomNoteColor({super.key, required this.colorIndex});
+class CustomNoteColor extends StatefulWidget {
+  const CustomNoteColor({
+    super.key,
+    required this.colorIndex,
+    required this.isSelected,
+    required this.onSelected,
+  });
   final int colorIndex;
+  final bool isSelected;
+  final VoidCallback onSelected;
+  @override
+  State<CustomNoteColor> createState() => _CustomNoteColorState();
+}
+
+class _CustomNoteColorState extends State<CustomNoteColor> {
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 12.0, horizontal: 4),
       child: GestureDetector(
-        onTap: () {},
-        child: CircleAvatar(
-          radius: 42,
-          backgroundColor: colorPallet[colorIndex],
-        ),
+        onTap: widget.onSelected,
+        child: widget.isSelected
+            ? CircleAvatar(
+                radius: 42,
+                backgroundColor: colorPallet[widget.colorIndex],
+                child: const Center(
+                  child: Icon(
+                    Icons.check,
+                    size: 44,
+                    color: Colors.white,
+                  ),
+                ),
+              )
+            : CircleAvatar(
+                radius: 42,
+                backgroundColor: colorPallet[widget.colorIndex],
+              ),
       ),
     );
   }
